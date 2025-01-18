e20b25c6e58abfb35285f708f7fb423e78e5a1ce
Carlos Vergara <carlosarturo.agelvisvergara@connect.qut.edu.au>
2025-01-18 18:22:09 +1100

    - Implementing Category and MenuItem models and serializers. - Creating migration files

--- ECommerceAPI/models.py
+++ ECommerceAPI/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class MenuItems(models.Model):
+    title = models.CharField(max_length=500)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    featured = models.BooleanField(db_default=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
