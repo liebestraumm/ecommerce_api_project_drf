3f78cd71171159e3da626af58e95c5d1a75a2cc9
Carlos Vergara <carlosarturo.agelvisvergara@connect.qut.edu.au>
2025-01-20 10:57:11 +1100

    Implementing user group management class views

--- ECommerceAPI/views.py
+++ ECommerceAPI/views.py
@@ -1,29 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, permissions, status, generics
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.views import APIView
 from .models import MenuItems, Cart
 from .serializers import MenuItemSerializer, CartSerializer
 from .custom_permissions import *
-from rest_framework.filters import OrderingFilter, SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.pagination import PageNumberPagination
+from django.contrib.auth.models import User, Group
+
 
 class StandardResultsPagination(PageNumberPagination):
     page_size = 3
-    page_size_query_param = 'page_size'
+    page_size_query_param = "page_size"
     max_page_size = 100
+
 
 class MenuItemsViewSet(viewsets.ModelViewSet):
     """
     A ModelViewSet for handling MenuItems with filtering, searching, ordering, and pagination.
     """
+
     queryset = MenuItems.objects.all()
     serializer_class = MenuItemSerializer
     permission_classes = [permissions.IsAuthenticated]  # Default permissions
     filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
-    filterset_fields = ['price', 'title', 'category']  # Fields to filter by
-    ordering_fields = ['price', 'id']  # Fields to order by
-    search_fields = ['title', 'category']  # Fields to search by
+    filterset_fields = ["price", "title", "category"]  # Fields to filter by
+    ordering_fields = ["price", "id"]  # Fields to order by
+    search_fields = ["title", "category"]  # Fields to search by
     pagination_class = StandardResultsPagination
 
     def get_permissions(self):
@@ -70,3 +75,96 @@ class CartmanagementView(generics.ListCreateAPIView, generics.DestroyAPIView):
         return Response(
             {"message": "Cart is already empty."}, status=status.HTTP_200_OK
         )
+
+
+class ManagerAPIView(APIView):
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [IsManager()]
+        return [permissions.IsAuthenticated()]
+
+    def get(self, request):
+        manager_group = Group.objects.get(name="Manager")
+        manager = manager_group.user_set.all()
+        data = [
+            {"id": user.id, "username": user.username, "email": user.email}
+            for user in manager
+        ]
+        return Response(data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        user_id = request.data.get("user_id")
+        user = get_object_or_404(User, id=user_id)
+        manager_group = Group.objects.get(name="Manager")
+        manager_group.user_set.add(user)
+        return Response(
+            {"message": f"User {user.username} added to the manager group"},
+            status=status.HTTP_201_CREATED,
+        )
+
+
+class ManagerDetailView(APIView):
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [IsManager()]
+        return [permissions.IsAuthenticated()]
+
+    def delete(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        manager_group = Group.objects.get(name="Manager")
+        if user in manager_group.user_set.all():
+            manager_group.user_set.remove(user)
+            return Response(
+                {"message": f"User {user.username} remove from the manager group"},
+                status=status.HTTP_200_OK,
+            )
+
+        return Response(
+            {"error": "User not found in manager group"},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+
+
+class ManagerDeliveryCrewView(APIView):
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [IsManager()]
+        return [permissions.IsAuthenticated()]
+
+    def get(self, request):
+        delivery_crew = Group.objects.get(name="DeliveryCrew")
+        delivery_boys = delivery_crew.user_set.all()
+        data = [
+            {"id": user.id, "username": user.username, "email": user.email}
+            for user in delivery_boys
+        ]
+        return Response(data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        user_id = request.data.get("user_id")
+        user = get_object_or_404(User, id=user_id)
+        manager_group = Group.objects.get(name="DeliveryCrew")
+        manager_group.user_set.add(user)
+        return Response(
+            {"message": f"User {user.username} added to the Delivery Crew group"},
+            status=status.HTTP_201_CREATED,
+        )
+
+
+class ManagerDeliveryCrewDetailView(APIView):
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [IsManager()]
+        return [permissions.IsAuthenticated()]
+
+    def delete(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        manager_delivery_group = Group.objects.get(name="Delivery Crew")
+        if user in manager_delivery_group.user_set.all():
+            manager_delivery_group.user_set.remove(user)
+            return Response(
+                {
+                    "message": f"User {user.username} remove from the Delivery Crew group"
+                },
+                status=status.HTTP_200_OK,
+            )
