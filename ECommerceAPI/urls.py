7bad33785c2cc8c432c8c18f523235a7912b28c8
Carlos Vergara <carlosarturo.agelvisvergara@connect.qut.edu.au>
2025-01-21 19:45:43 +1100

    Adding USer management endpoints

--- ECommerceAPI/urls.py
+++ ECommerceAPI/urls.py
@@ -6,6 +6,22 @@ router = DefaultRouter()
 router.register(r"menu-items", MenuItemsViewSet, basename="menu-items")
 
 urlpatterns = [
+    path("groups/manager/users", ManagerAPIView.as_view(), name="manager-users"),
+    path(
+        "groups/manager/users/<int:pk>",
+        ManagerDetailView.as_view(),
+        name="manager-users",
+    ),
+    path(
+        "groups/delivery-crew/users",
+        ManagerDeliveryCrewView.as_view(),
+        name="delivery-crew-users",
+    ),
+    path(
+        "groups/delivery-crew/users/<int:pk>",
+        ManagerDeliveryCrewDetailView.as_view(),
+        name="delivery-crew-users",
+    ),
     path("cart/menu-items", CartmanagementView.as_view(), name="cart/menu-items"),
     *router.urls,
 ]
